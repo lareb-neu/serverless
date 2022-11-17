9605e97a866dbe28440e8a69136d1c26bdd1a000
larebkhan1996 <khan.la@northeastern.edu>
2022-11-16 23:46:43 -0500

    update sednemail.py

--- sendemail.py
+++ sendemail.py
@@ -1,39 +1,77 @@
+from botocore.exceptions import ClientError
 import json
 import boto3
+def handler(event, context):
+    username=json.loads(event['Records'][0]['Sns']['Message'])['Username']
+    token=json.loads(event['Records'][0]['Sns']['Message'])['Subject']
+    send_email(username, token)
 
-def lambda_handler(event, context):
+def send_email(email,token):
+    SENDER = "no-reply@demo.larebkhan.me"
+
+    RECIPIENT = email
+
+    authlink = "http://demo.larebkhan.me/verify?email=" + email + "&token=" + token
+
+    print(authlink)
+
+
+    DESTINATION = {
+        'ToAddresses': [
+            RECIPIENT,
+        ]
+    }
+
+    AWS_REGION = "us-east-1"
+
+    SUBJECT = "Verification Email"
+
+    BODY_TEXT = ("Email verification for new user\r\n"
+                 "Details:\r\n"
+                 "\n"
+                 "Name: " + email + "\n"
+                 "\n"
+                 "Verifying email id: " + email + "\r\n"
+                 "\r\n"
+                 "Use the link provided below to verify yourself:\r\n"
+                 "Verify: " + authlink
+                 )
+
+    CHARSET = "UTF-8"
     
-    ses = boto3.client('ses')
+    client = boto3.client('ses', region_name='us-east-1',aws_access_key_id='AKIAZAXXVSVN3BCKCQMV',
+                                  aws_secret_access_key='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')
 
-    body = """
-	    Hello and welcome to the SES Lambda Python Demo.
-	
-	    Regards,
-	    NKT Studios
-    """
+    try:
+        response = client.send_email(
+            Destination=DESTINATION,
+            Message={
+                'Body': {
+                    # 'Html': {
+                    #     'Charset': CHARSET,
+                    #     'Data': BODY_,
+                    # },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+         )
 
-    ses.send_email(
-	    Source = 'noreply@demo.larebkhan.me',
-	    Destination = {
-		    'ToAddresses': [
-			    'larebkhan96@gmail.com'
-		    ]
-	    },
-	    Message = {
-		    'Subject': {
-			    'Data': 'SES Demo',
-			    'Charset': 'UTF-8'
-		    },
-		    'Body': {
-			    'Text':{
-				    'Data': body,
-				    'Charset': 'UTF-8'
-			    }
-		    }
-	    }
-    )
-    
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:")
+        print(response['MessageId'])
+        print(RECIPIENT)
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Successfully sent email from Lambda using Amazon SES')
+        'body': json.dumps(response)
     }
